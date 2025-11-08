fcc99f9201ee245b24c13a4361903b43c449061c
Lg151621 <destrok21@gmail.com>
2025-11-08 14:14:17 -0700

    Fix API baseURL to use backend URL

--- src/lib/dataSource.tsx
+++ src/lib/dataSource.tsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const baseURL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_API_URL || "https://cst-391-music-app.vercel.app";
 
 const dataSource = axios.create({
-  baseURL: baseURL, 
+  baseURL,
   headers: { "Content-Type": "application/json" },
 });
 
